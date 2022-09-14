17a50965300af78ab4620e21460f6839d04effda
Thanystos <guichardanthony@live.fr>
2022-09-14 14:55:38 +0200

    - Retrait de balises div superflues - Stockage des données liées aux produits dans le LS (utilisation d'une map)

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,7 @@
 const item = document.getElementsByClassName('item')[0];
 
+const productArticleElt = document.createElement('article');
+
 const str = document.location.href;
 const url = new URL(str);
 const id = url.searchParams.get('id');
@@ -14,10 +16,17 @@ async function getProduct() {
 }
 
 function showProduct(product) {
-    const { colors, name, price, imageUrl, description, altTxt } = product;
-    console.log(product);
+    const { _id, colors, name, price, imageUrl, description, altTxt } = product;
+    let cart;
+    if (JSON.parse(localStorage.getItem('products')) != null) {
+        cart = new Map(Object.entries(JSON.parse(localStorage.getItem('products'))));
+        console.log('Map après LS: ');
+        console.log(cart);
+    }
+    else {
+        cart = new Map();
+    }
 
-    const productArticleElt = document.createElement('article');
     productArticleElt.innerHTML = 
     `
         <div class="item__img">
@@ -61,26 +70,47 @@ function showProduct(product) {
             </label>
             <input id="quantity" type="number" name="itemQuantity" min="1" max="100" value="0">
         </div>
-        </div>
         <div class="item__content__addButton">
             <button id="addToCart">
                 Ajouter au panier
             </button>
         </div>
-    </div>
     `
     colors.forEach((color) => {
         productArticleElt.querySelector('#colors').appendChild(getColor(color));
     })
     item.appendChild(productArticleElt);
-}
 
-function getColor(color) {
-    const colorsElt = document.createElement('option');
-    colorsElt.value = color;
-    colorsElt.innerHTML =
-        `
-            ${color}
-        `
-    return colorsElt;
+    function getColor(color) {
+        const colorsElt = document.createElement('option');
+        colorsElt.value = color;
+        colorsElt.innerHTML =
+            `
+                ${color}
+            `
+        return colorsElt;
+    }
+
+    productArticleElt.querySelector('#addToCart').addEventListener('click', () => {
+        if(((productArticleElt.querySelector('#colors').value) != '') 
+        && ((productArticleElt.querySelector('#quantity').value > 0) && (productArticleElt.querySelector('#quantity').value <= 100))) {
+            if((cart.get(`${_id}_${productArticleElt.querySelector('#colors').value}`) != undefined) && (cart.get(`${_id}_${productArticleElt.querySelector('#colors').value}`).color == productArticleElt.querySelector('#colors').value)) {
+                cart.set(`${_id}_${cart.get(`${_id}_${productArticleElt.querySelector('#colors').value}`).color}`, {id: _id, color: productArticleElt.querySelector('#colors').value, quantity: parseInt(cart.get(`${_id}_${productArticleElt.querySelector('#colors').value}`).quantity, 10) + parseInt(productArticleElt.querySelector('#quantity').value, 10)});
+                console.log('Map avant LS: ');
+                console.log(cart);
+            }
+            else {
+                cart.set(`${_id}_${productArticleElt.querySelector('#colors').value}`, {id: _id, color: productArticleElt.querySelector('#colors').value, quantity: parseInt(productArticleElt.querySelector('#quantity').value, 10)});
+                console.log('Map avant LS: ');
+                console.log(cart);
+            }
+            localStorage.setItem('products', JSON.stringify(Object.fromEntries(cart)));
+            console.log('Object dans LS: ' + JSON.stringify(Object.fromEntries(cart)));
+
+            alert('Article(s) ajouté(s) au panier !');
+        }
+        else {
+            alert('Veuillez choisir une couleur et une quantité valide.')
+        }
+    })
 }
